2f370c289e3a5c829acaca29e96aa33bfac3785d
Preet Kumar <113340889+preettulas@users.noreply.github.com>
2022-11-12 11:50:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const http = require("http");
 const fs = require("fs");
 const args = require('minimist')(process.argv.slice(2));
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
+let home_Content = "";
+let project_Content = "";
+let registration_Content = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
-  homeContent = home;
+  home_Content = home;
 });
 
 fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err;
   }
-  projectContent = project;
+  project_Content = project;
 });
 
 fs.readFile("registration.html", (err, registration) => {
   if (err) {
     throw err;
   }
-  registrationContent = registration;
+  registration_Content = registration;
 });
 
 fs.readFile("home.html", (err, home) => {
